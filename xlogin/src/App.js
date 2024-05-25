f6f405001eb252235b1c649cf6c2618dfce1a485
aryan <aycool19@gmail.com>
2024-05-25 16:39:28 +0530

    login fun added

---
+++ xlogin/src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react'
+
+function App() {
+  let [user, setUser] = useState({"name":'', "password":''})
+  let [validated, setvalidated] = useState(true)
+  let [loggedIn, setloggedIn] = useState(false)
+  let handleInputData = (e) => {
+    let {name, value} = e.target
+    setUser((prevValue) => (
+      {...prevValue, [name]:value}
+    ))
+    console.log(user)
+  }
+  let formUi = <form onSubmit={validateForm}>
+  <label>Username:</label> 
+  <input type="text" placeholder="Username" name ="name" value={user.name}  onChange={handleInputData} required/><br/>
+  <label>Password: </label>
+  <input type="password" name="password" value={user.password} onChange={handleInputData} placeholder="Password"  required/><br/>
+  <button type="submit">Submit</button>
+</form>
+  function validateForm(e){
+    e.preventDefault()
+    if(user.name === "user" && user.password === "password"){
+      setvalidated(true)
+      setloggedIn(true)
+    }else{
+      setvalidated(false)
+      setloggedIn(false)
+    }
+    
+  }
+
+ 
+  return (
+    <div>
+      <h2> Login Page</h2>
+      {loggedIn ? ("Welcome , user") : 
+       (validated ? formUi : <div>
+        <h4>Invalid useranme or password</h4>
+        {formUi}
+       </div>)
+      }
+      
+    </div>
+  );
+}
+
+export default App;
